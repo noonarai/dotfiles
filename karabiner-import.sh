20cc0d6df52d312c5a5e26b76963002fcb8e2d28
shogo <noonarai@gmail.com>
2016-05-11 14:18:17 +0800

    update キーリピート早くした

--- karabiner-import.sh
+++ karabiner-import.sh
@@ -10,7 +10,7 @@ $cli set general.disable_internal_keyboard_if_external_keyboard_exsits 1
 /bin/echo -n .
 $cli set remap.jis_command2eisuukana_prefer_command 1
 /bin/echo -n .
-$cli set repeat.wait 33
+$cli set repeat.wait 10
 /bin/echo -n .
 $cli set remap.simple_vi_mode 1
 /bin/echo -n .
