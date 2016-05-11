31b623319d81038c52a9aaed3df8c13dd8f3f6d9
shogo <noonarai@gmail.com>
2016-05-11 14:11:07 +0800

    update

--- karabiner-import.sh
+++ karabiner-import.sh
@@ -2,20 +2,20 @@
 
 cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
 
-$cli set option.extra_cursor_optionL_kjhl 1
-/bin/echo -n .
-$cli set repeat.wait 20
-/bin/echo -n .
-$cli set repeat.initial_wait 200
-/bin/echo -n .
-$cli set option.vimode_control_gG 1
+$cli set repeat.initial_wait 250
 /bin/echo -n .
 $cli set remap.jis_command2eisuukana_prefer_eisuukana 1
 /bin/echo -n .
+$cli set general.disable_internal_keyboard_if_external_keyboard_exsits 1
+/bin/echo -n .
 $cli set remap.jis_command2eisuukana_prefer_command 1
 /bin/echo -n .
-$cli set option.vimode_control_bf 1
+$cli set repeat.wait 33
+/bin/echo -n .
+$cli set remap.simple_vi_mode 1
 /bin/echo -n .
 $cli set remap.fkeys_to_consumer_f7 1
+/bin/echo -n .
+$cli set remap.mouse_keys_mode_2 1
 /bin/echo -n .
 /bin/echo
