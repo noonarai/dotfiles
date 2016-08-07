33e5fb1011b81e01b27208a9006bff767b9deec9
shogo <noonarai@gmail.com>
2016-08-08 00:56:09 +0900

    fix いらない設定消した

--- karabiner-import.sh
+++ karabiner-import.sh
@@ -2,20 +2,14 @@
 
 cli=/Applications/Karabiner.app/Contents/Library/bin/karabiner
 
-$cli set repeat.initial_wait 250
+$cli set remap.jis_command2eisuukana_prefer_command 1
 /bin/echo -n .
 $cli set remap.jis_command2eisuukana_prefer_eisuukana 1
 /bin/echo -n .
-$cli set general.disable_internal_keyboard_if_external_keyboard_exsits 1
-/bin/echo -n .
-$cli set remap.jis_command2eisuukana_prefer_command 1
-/bin/echo -n .
-$cli set repeat.wait 10
-/bin/echo -n .
 $cli set remap.simple_vi_mode 1
 /bin/echo -n .
-$cli set remap.fkeys_to_consumer_f7 1
+$cli set repeat.initial_wait 200
 /bin/echo -n .
-$cli set remap.mouse_keys_mode_2 1
+$cli set repeat.wait 13
 /bin/echo -n .
 /bin/echo
